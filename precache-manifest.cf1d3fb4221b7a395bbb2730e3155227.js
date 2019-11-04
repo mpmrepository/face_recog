20589e442dc1f64e79ed7c198cb791d24e37cb36
mmcallen <matthewphilip.mcallen@burberry.com>
2019-11-04 21:32:24 +0000

    Updates

---
+++ precache-manifest.cf1d3fb4221b7a395bbb2730e3155227.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d273bb945a657c05aab0fcd01b8fa9d",
+    "url": "/facerecog/index.html"
+  },
+  {
+    "revision": "e0b2408c5ff856c2ccd6",
+    "url": "/facerecog/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b170ac0d87db1bf46af0",
+    "url": "/facerecog/static/css/main.bcdc52f6.chunk.css"
+  },
+  {
+    "revision": "e0b2408c5ff856c2ccd6",
+    "url": "/facerecog/static/js/2.34b56b8a.chunk.js"
+  },
+  {
+    "revision": "b170ac0d87db1bf46af0",
+    "url": "/facerecog/static/js/main.9e9722fe.chunk.js"
+  },
+  {
+    "revision": "a5015c0355c343f7a8c0",
+    "url": "/facerecog/static/js/runtime-main.90d67e58.js"
+  },
+  {
+    "revision": "504f81375c4fb3868ae2f7503a73ecec",
+    "url": "/facerecog/static/media/logo.504f8137.svg"
+  }
+]);

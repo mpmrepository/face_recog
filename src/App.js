79ba3108333b0672c8c2f6cfe22e4cb8b27d6d8a
mmcallen <matthewphilip.mcallen@burberry.com>
2019-11-21 20:27:40 +0000

    making sure its up to date

--- src/App.js
+++ src/App.js
@@ -26,7 +26,11 @@ class App extends Component {
       isSignedIn: false
     }
   }
-
+  componentDidMount() {
+    fetch('http://localhost:8000')
+    .then(response => response.json())
+    .then(console.log)
+  }
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
